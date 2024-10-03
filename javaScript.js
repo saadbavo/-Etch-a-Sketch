c85a7c2744d1e98936f2a59b0188ac9df1e4dfac
saadbavo <[saadhmk13@gmail.com]>
2024-10-03 22:17:11 +0100

    Add fonction for that creaaat 16 divs for rows and athore for colms

---
+++ javaScript.js
@@ -0,0 +1,19 @@
+
+function makeDivs(){
+   
+    for(let i=0; i<16; i++ ){
+        const container = document.querySelector(".container");
+        const contentRows = document.createElement("div");
+        contentRows.classList.add("rows");
+        container.appendChild(content)
+
+    }
+    for(let i=0; i<16; i++ ){
+        const container = document.querySelector(".container");
+        const contentColms = document.createElement("div");
+        contentColms.classList.add("colms");
+        container.appendChild(content)
+
+    }
+
+}
